f2e038a03939d72c8841c663ce8051bcfe8b2539
DaoLongVu20130466 <20130466@st.hcmuaf.edu.vn>
2023-05-24 22:27:12 +0700

    tesst

--- app/src/main/java/com/project/android/MainActivity.java
+++ app/src/main/java/com/project/android/MainActivity.java
@@ -1,17 +1,18 @@
 package com.project.android;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
         myRef.setValue("Hello, World!");
