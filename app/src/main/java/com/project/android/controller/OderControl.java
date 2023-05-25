5566f41b157c083d92b0f5ca690f810189d2122b
DaoLongVu20130466 <20130466@st.hcmuaf.edu.vn>
2023-05-26 00:54:53 +0700

    update

--- app/src/main/java/com/project/android/controller/OderControl.java
+++ app/src/main/java/com/project/android/controller/OderControl.java
@@ -8,6 +8,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.project.android.model.Oder;
 import com.project.android.model.Product;
 
@@ -29,26 +30,11 @@ public class OderControl {
         ArrayList<Oder> list = new ArrayList<>();
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://quanlyquancom-default-rtdb.asia-southeast1.firebasedatabase.app");
         DatabaseReference myRef = database.getReference("Oder");
-        myRef.child("Oder").addChildEventListener(new ChildEventListener() {
+        myRef.child("Oder").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Oder rs = snapshot.getValue(Oder.class);
                 list.add(rs);
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
             }
 
             @Override
